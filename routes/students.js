8905c0e51507844bce5af31eed3a8af3d57f0be3
Sam <samjoyce18@yahoo.com>
2021-09-07 17:00:58 +0100

    removed excess code which is for the next branch

--- routes/students.js
+++
@@ -1,9 +0,0 @@
-import Router from 'express';
-
-const router = Router(); 
-
-router.get('/', (req, res) => res.status(200).send({
-  message: "Here is all the students!"
-}))
-
-export default router;
