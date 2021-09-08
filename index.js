ad7133eb2025e977f1d0887ccc6be1ff9c66ef57
Sam <samjoyce18@yahoo.com>
2021-09-08 10:17:26 +0100

    ready to move

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import router from './routes/students.js';
 const app = express();
 const port  = process.env.PORT || 3000;
 
+app.use('/api/students', router);
 app.get('/api', (req, res) => {
    res.send("Welcome to my API!");
 })
+app.get("*", (req, res) => res.status(404).send("There is no content at this route."));
 
-app.use('/api/students', router);
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
--- routes/students.js
+++ routes/students.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => res.status(200).send({
   message: "Here is all the students!"
 }))
 
+router.get("/:id", (req, res) => res.status(200).send({
+  message: "Got one student!"
+}));
+
+router.post("/", (req, res) => res.status(201).send({
+  message: "Created a new student!"
+}));
+
+router.delete("/:id", (req, res) => res.status(204).send({
+  message: "One student down!"
+}));
+
 export default router;
